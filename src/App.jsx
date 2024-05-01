3810ccb8eaea3f0d1a8f9e16cdc8314b71d1b288
Patrickodida <patrick.o.odida@gmail.com>
2024-05-01 08:15:15 +0300

    SetUp tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
-
+import './index.css'
 
 function App(){
     return (
-        <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
-        </BrowserRouter>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import React from "react";
+
 function Home(){
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1 className="text-dark-light mt-5 text-center md:text-xl lg:text-base xl:text-xl lg:text-left">
+        Home Page
+      </h1>
         </div>
     )
 }
