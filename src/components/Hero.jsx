33f4c7c7b0fcde70bdf0a6f4fbe8f295bfa40084
Patrickodida <patrick.o.odida@gmail.com>
2024-05-01 18:56:21 +0300

    Add styles to the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,23 +4,24 @@ function Hero() {
   return (
     <div>
       <section className=" hero-section">
-        <div className="hero-over-lay"></div>
-        <div className="hero-content">
-          <div>
+        <div className="hero-over-lay">
+        </div>
+        <div className="hero-content justify-center gap-10 w-4/5 max-w-7xl mx-auto">
+          <div className="text-5xl w-2/4 font-bold leading-tight">
             <h1>Get Your Ticket Online Easy and Safely</h1>
           </div>
-          <div>
-            <h2>Choose Your Ticket</h2>
-            <form>
+          <div className="w-2/4">
+            <h2 className="text-base pb-2.5 font-bold">Choose Your Ticket</h2>
+            <form className="p-5 form-center bg-white rounded-lg">
               <div className="form-row">
-                <input type="text" placeholder="From" />
+                <input type="text" placeholder="From" className="mb-1 text-blue-900 rounded" />
 
-                <input type="text" placeholder="To" />
+                <input type="text" placeholder="To" className="text-blue-900 rounded" />
               </div>
 
-              <input type="date" />
+              <input type="date" className="text-blue-900 rounded" />
 
-              <button type="submit">Find Ticket</button>
+              <button type="submit" className="bg-blue-900 rounded-lg">Find Ticket</button>
             </form>
           </div>
         </div>
