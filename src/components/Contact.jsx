168577541389086deddc72cfa0479c28287266d3
Patrickodida <patrick.o.odida@gmail.com>
2024-05-02 21:32:55 +0300

    Add Contact section structure

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Contact(){
     return (
         <div>
-            <p>Contact Us</p>
+            <section className="contact-section">
+                <h2 className="text-4xl text-center my-6">Contact Us</h2>
+            </section>
         </div>
     )
 }
