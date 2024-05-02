b8b136258eab533bdf54cbaae57e69834f05fa0c
Patrickodida <patrick.o.odida@gmail.com>
2024-05-02 17:19:17 +0300

    Add styles to the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,14 +3,14 @@ import React from "react";
 function About(){
     return (
         <div>
-            <section className="about-section">
-                <h2>About</h2>
-                <p>Welcome to KwataBus – your go-to platform for hassle-free bus booking experiences! We're thrilled to have you aboard.</p>
-                <p>At KwataBus, we understand the importance of seamless travel. Whether you're commuting for work, embarking on an adventure, or simply exploring new destinations, we believe that every journey should be comfortable, convenient, and memorable. That's why we've crafted a user-friendly platform that puts your travel needs first.</p>
-                <p>Founded on the principles of reliability, efficiency, and customer satisfaction, KwataBus aims to revolutionize the way you book bus tickets. With just a few clicks, you can easily compare schedules, select your preferred routes, and secure your seats – all from the comfort of your home or on-the-go via our mobile app.</p>
-                <p>But KwataBus is more than just a booking platform – we're your trusted travel companion. Our dedicated team works tirelessly to ensure that every aspect of your journey is taken care of, from reservation to arrival. Whether you have questions about your booking, need assistance during your trip, or simply want travel tips and recommendations, our customer support team is always here to help.</p>
-                <p>Safety is our top priority at KwataBus. We partner with reputable bus operators who adhere to strict safety standards, so you can rest assured knowing that you're in good hands every step of the way. From well-maintained vehicles to experienced drivers, we go above and beyond to prioritize your well-being.</p>
-                <p>As passionate advocates for sustainable travel, we're committed to minimizing our environmental footprint. By promoting bus travel as an eco-friendly alternative to other modes of transportation, we're doing our part to create a greener, more sustainable future for generations to come.</p>
+            <section className="about-section w-4/5 max-w-screen-xl m-auto">
+                <h2 className="text-4xl text-center my-6">About</h2>
+                <p className="mb-4 text-base">Welcome to KwataBus – your go-to platform for hassle-free bus booking experiences! We're thrilled to have you aboard.</p>
+                <p className="mb-4 text-base">At KwataBus, we understand the importance of seamless travel. Whether you're commuting for work, embarking on an adventure, or simply exploring new destinations, we believe that every journey should be comfortable, convenient, and memorable. That's why we've crafted a user-friendly platform that puts your travel needs first.</p>
+                <p className="mb-4 text-base">Founded on the principles of reliability, efficiency, and customer satisfaction, KwataBus aims to revolutionize the way you book bus tickets. With just a few clicks, you can easily compare schedules, select your preferred routes, and secure your seats – all from the comfort of your home or on-the-go via our mobile app.</p>
+                <p className="mb-4 text-base">But KwataBus is more than just a booking platform – we're your trusted travel companion. Our dedicated team works tirelessly to ensure that every aspect of your journey is taken care of, from reservation to arrival. Whether you have questions about your booking, need assistance during your trip, or simply want travel tips and recommendations, our customer support team is always here to help.</p>
+                <p className="mb-4 text-base">Safety is our top priority at KwataBus. We partner with reputable bus operators who adhere to strict safety standards, so you can rest assured knowing that you're in good hands every step of the way. From well-maintained vehicles to experienced drivers, we go above and beyond to prioritize your well-being.</p>
+                <p className="mb-4 text-base">As passionate advocates for sustainable travel, we're committed to minimizing our environmental footprint. By promoting bus travel as an eco-friendly alternative to other modes of transportation, we're doing our part to create a greener, more sustainable future for generations to come.</p>
             </section>
         </div>
     )
