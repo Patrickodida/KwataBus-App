2fa6e6fcc62d429edb27845bc6be4ab8c765e505
Patrickodida <patrick.o.odida@gmail.com>
2024-05-03 13:42:30 +0300

    Add Styles to the ticket steps section

--- src/components/TicketSteps.jsx
+++ src/components/TicketSteps.jsx
@@ -5,38 +5,38 @@ function TicketSteps(){
         <div>
             <section className="w-4/5 max-w-screen-xl m-auto">
         <h2 className="text-4xl text-center my-6">Get Your Tickets with Just 3 Steps</h2>
-        <div className="card flex justify-center gap-10  ">
-          <div className="w-2/4">
-            <h3>Search Your Bus</h3>
+        <div className="card flex justify-between items-center gap-10  ">
+          <div className="icons-container border rounded-lg">
+            <h4>1</h4>
+            <i class='bx bx-search-alt-2 icon'></i>
           </div>
-          <div>
-            <h3>Search Your Bus</h3>
+          <div className="text-container w-2/4">
             <p>
               Choose your destination, origin, date, and then search for buses
             </p>
           </div>
         </div>
 
-        <div className="card flex justify-content gap-10  ">
-          <div>
-            <h3>Search Your Bus</h3>
+        <div className="card flex justify-between items-center gap-10  ">
+          <div className="text-container w-2/4">
             <p>
-              Choose your destination, origin, date, and then search for buses
+              Select a seat and make a booking
             </p>
           </div>
-          <div className="w-2/4">
-            <h3>Search Your Bus</h3>
+          <div className="icons-container border rounded-lg">
+            <h4>2</h4>
+            <i class='bx bx-search-alt-2 icon'></i>
           </div>
         </div>
 
-        <div className="card flex justify-content gap-10  ">
-          <div className="w-2/4">
-            <h3>Search Your Bus</h3>
+        <div className="card flex justify-between items-center gap-10  ">
+          <div className="icons-container border rounded-lg">
+            <h4>3</h4>
+            <i class='bx bx-search-alt-2 icon'></i>
           </div>
-          <div>
-            <h3>Search Your Bus</h3>
+          <div className="text-container w-2/4">
             <p>
-              Choose your destination, origin, date, and then search for buses
+              Pay for seat using payment options provided
             </p>
           </div>
         </div>
