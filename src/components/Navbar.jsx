cd9343635110304743f0fd5af8920a93be9cd1e7
Patrickodida <patrick.o.odida@gmail.com>
2024-05-05 12:33:06 +0300

    Modify hover effect on the nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,28 +7,28 @@ function Navbar() {
       <section className="bg-white text-blue-900 fixed top-0 left-0 right-0 z-50 w-full">
         <div className="flex items-center justify-between m-auto w-4/5 max-w-screen-xl py-4">
           <Link href="/" className="site-title font-bold text-2xl">
-          Kwata<span>Bus</span>
+          Kwata<span style={{color:'#e3bf00'}}>Bus</span>
         </Link>
         <nav className="flex items-center w-full">
         <ul className="flex m-auto font-bold ">
           <li>
-            <Link className="hover:text-yellow-600" href="/home">HOME</Link>
+            <Link className="nav-links"  href="/home">HOME</Link>
           </li>
           <li className="mx-8">
-            <Link className="hover:text-yellow-600" href="/about">ABOUT</Link>
+            <Link className="nav-links"  href="/about">ABOUT</Link>
           </li>
           <li>
-            <Link className="hover:text-yellow-600" href="/contact">CONTACT</Link>
+            <Link className="nav-links"  href="/contact">CONTACT</Link>
           </li>
         </ul>
         <ul className="flex items-center">
           <li>
-            <Link href="/help">Help</Link>
+            <Link className="nav-links" href="/help">Help</Link>
           </li>
           <li className="active  ml-8">
-            <Link href="/login" style={{ whiteSpace: 'nowrap' }}>Log In</Link>
+            <Link className="nav-links"href="/login" style={{ whiteSpace: 'nowrap' }}>Log In</Link>
           </li>
-        <button type="submit" className="rounded-2xl ml-8 font-normal px-4">
+        <button type="submit" className="button-link rounded-2xl ml-8 font-normal px-4">
             Sign up
         </button>
         </ul>
