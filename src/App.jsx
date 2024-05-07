cdabe1cfba4e576aa5aa03a85aa81169f86d0106
Patrickodida <patrick.o.odida@gmail.com>
2024-05-07 15:34:49 +0300

    Add the route path to different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Help from './pages/Help'
 import './index.css'
 
 function App(){
     return (
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="" element={<Login />} />
-                <Route path="" element={<Signup />} />
-                <Route path="" element={<Help />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/Signup" element={<Signup />} />
+                <Route path="/Help" element={<Help />} />
             </Routes>
     )
 }
