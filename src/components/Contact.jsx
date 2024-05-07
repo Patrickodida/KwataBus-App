b182d60aeb5c892319e2a6e47c53ef0d785391e9
Patrickodida <patrick.o.odida@gmail.com>
2024-05-07 18:20:08 +0300

    Add responsiveness on the contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,8 @@ function Contact() {
     <div>
       <section className="contact-section w-[80%] m-auto">
         <h2 className="text-4xl text-center mb-6 font-bold">Contact Us</h2>
-        <div className="contact-container flex justify-around items-center gap-10">
-          <div className="form-section w-[50%]">
+        <div className="contact-container text-center md:flex md:justify-around md:items-center md:gap-10">
+          <div className="form-section w-full mb-[3em] md:w-[50%]">
             <h3 className="mb-[24px] font-bold">Get In Touch</h3>
           <div className="form">
             <input className="w-full text-blue-900 rounded p-[0.425em] mb-[24px] border border-gray-300 text- [#061f77] focus:outline-none" type="text" placeholder="Your Name" />
