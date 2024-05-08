3e6e024657b6252ea6299fa4af8595a074c69a3f
Patrickodida <patrick.o.odida@gmail.com>
2024-05-09 02:56:47 +0300

    Modify the cards in the ticket section

--- src/components/TicketSteps.jsx
+++ src/components/TicketSteps.jsx
@@ -6,35 +6,41 @@ import PayTicket from '../images/payticket.svg'
 function TicketSteps() {
   return (
     <div>
-      <section className="ticket-section w-4/5 max-w-screen-xl m-auto">
+      <section className="ticket-section w-4/5 max-w-screen-xl m-auto text-[#061f77]">
         <h2 className="text-4xl text-center my-6 font-bold">
           Get Your Tickets with Just 3 Steps
         </h2>
-        <div className="card flex justify-center items-center gap-[10%]">
-          <div className="icons-container w-1/2 h-auto flex justify-center text-[#061f77]">
-            <img src={SvgDest} className="w-1/2 h-auto"/>
+        <div className="card md:flex md:justify-center md:items-center md:gap-[10%] mb-12 border md:border-none shadow-md md:shadow-none ">
+          <div className="icons-container w-1/2 h-auto flex justify-center text-[#061f77]  rounded-lg p-4 w-full m-auto md:border md:shadow-md">
+            <span className="md:flex md:justify-center font-bold text-[#061f77] pb-[30px] hidden">1</span>
+            <img src={SvgDest} className="w-1/4 h-auto"/>
           </div>
-          <div className="text-container w-2/4">
+          <div className="text-container w-2/4  rounded p-4 w-full m-auto md:border md:shadow-md">
+            
             <p>
               Choose your destination, origin, date, and then search for buses
             </p>
           </div>
         </div>
 
-        <div className="card flex justify-center items-center gap-[10%]">
-          <div className="text-container w-2/4">
+        <div className="card md:flex md:justify-center md:items-center md:gap-[10%] mb-12 border md:border-none shadow-md md:shadow-none">
+          <div className="text-container w-2/4  rounded p-4 w-full m-auto md:border md:shadow-md">
+            
             <p>Select a seat and make a booking</p>
           </div>
-          <div className="icons-container w-1/2 h-auto flex justify-center">
+          <div className="icons-container w-1/2 h-auto flex justify-center text-[#061f77]  rounded-lg p-4 w-full m-auto md:border md:shadow-md">
+            <span className="md:flex md:justify-center font-bold text-[#061f77] pb-[30px] hidden">2</span>
             <img src={SvgChoose} className="w-1/2 h-auto"/>
           </div>
         </div>
 
-        <div className="card flex justify-center items-center gap-[10%]">
-          <div className="icons-container w-1/2 h-auto flex justify-center">
+        <div className="card md:flex md:justify-center md:items-center md:gap-[10%] mb-12 border md:border-none shadow-md md:shadow-none">
+          <div className="icons-container w-1/2 h-auto flex justify-center text-[#061f77]  rounded-lg p-4 w-full m-auto md:border md:shadow-md">
+            <span className="md:flex md:justify-center font-bold text-[#061f77] pb-[30px] hidden">3</span>
             <img src={PayTicket} className="w-1/2 h-auto"/>
           </div>
-          <div className="text-container w-2/4">
+          <div className="text-container w-2/4  rounded p-4 w-full m-auto md:border md:shadow-md">
+            
             <p>Pay for seat using payment options provided</p>
           </div>
         </div>
