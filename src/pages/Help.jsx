981811e9d5df474d26b19a3bd49ffc0ac10c852e
mugisha <mohamugi@gmail.com>
2024-05-09 01:30:41 +0300

    size the input fields

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -12,7 +12,7 @@ function Help() {
     }
     return (
         <div>
-            <div className="flex justify-between p-8 m-auto items-center w-full font-bold bg-[#061f77]">
+            <div className="flex justify-between p-8 m-auto items-center  font-bold bg-[#061f77]">
                 <Link href="/" className="site-title font-bold text-2xl">
                     Kwata<span style={{ color: "#e3bf00" }}>Bus</span>
                 </Link>
@@ -34,7 +34,7 @@ function Help() {
             
             <form action="" className="h-[100vh] text-center">
                 <h1 className="text-center p-8 m-auto text-[#061f77] font-bold text-3xl">FREQUENTLY ASKED QUESTIONS (FAQS)</h1>
-                <select value={selectedValue} onChange={handleChange} className="p-8 m-auto  text-center border-4">
+                <select value={selectedValue} onChange={handleChange} className="p-6 m-auto w-4/5 max-w-screen-xl text-center border-4 rounded-lg">
                     <option value="question1">HOW DO I BUY TICKETS?</option>
                     <option value="question2">Lorem ipsum dolor, sit amet consectetur adipisicing elit?</option>
                     <option value="question3">Lorem ipsum dolor, sit amet consectetur adipisicing elit?</option>
@@ -44,7 +44,7 @@ function Help() {
                 <br />
                 <br />
 
-                <select value={selectedValue} onChange={handleChange} className="p-8 m-auto text-center border-4">
+                <select value={selectedValue} onChange={handleChange} className="p-6 m-auto w-4/5 max-w-screen-xl text-center border-4 rounded-lg">
                     <option value="question1">CAN THE TICKET BE SENT TO ME VIA SMS?</option>
                     <option value="question2">Lorem ipsum dolor, sit amet consectetur adipisicing elit?</option>
                     <option value="question3">Lorem ipsum dolor, sit amet consectetur adipisicing elit?</option>
@@ -54,7 +54,7 @@ function Help() {
                 <br />
                 <br />
 
-                <select value={selectedValue} onChange={handleChange} className="p-8 m-auto  text-center border-4">
+                <select value={selectedValue} onChange={handleChange} className="p-6 m-auto w-4/5 max-w-screen-xl  text-center border-4 rounded-lg">
                     <option value="question1">I HAVE PURCHASED A TICKET BUT HAVE NOT RECEIVED A MESSAGE?</option>
                     <option value="question2">Lorem ipsum dolor, sit amet consectetur adipisicing elit?</option>
                     <option value="question3">Lorem ipsum dolor, sit amet consectetur adipisicing elit?</option>
