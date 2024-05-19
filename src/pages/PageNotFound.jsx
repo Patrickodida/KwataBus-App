150a9c36fe4c247ad324af506bd3f3b22b7b1a5c
Patrickodida <patrick.o.odida@gmail.com>
2024-05-19 19:53:55 +0300

    Add the 404 page with styles and link to home page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom"
-import SvgSmartphone from "../images/smartphone.svg";
-import SvgPeople from "../images/bookSeat.svg";
+import { Link } from "react-router-dom";
 
 function PageNotFound() {
   return (
     <div>
-      <section className="my-[3em]">
-        <div className="w-[80%] m-auto flex flex-col justify-center">
-          <h1 className="text-[3rem] text-center">404 Page Not Found</h1>
-        <p>The page you are looking for might have been removed, had it's name changed or its temporarily unavailable</p>
+      <section className="py-[5em] w-[80%] m-auto text-center ">
+        <div className="">
+            <h1 className="text-[8rem] text-[#061f77] font-bold">Oops!</h1>
+          <h2 className="text-[2.5rem] mb-4">404 - Page Not Found</h2>
+          <p className="text-[0.875rem] max-w-md m-auto mb-4 ">
+            The page you are looking for might have been removed, had it's name
+            changed or its temporarily unavailable
+          </p>
         </div>
-        <Link to="/">Go To Home Page</Link>
+        <div className="text-[white] bg-[#061f77] w-[25%] flex justify-center m-auto py-4 rounded-[4em]">
+          <Link to="/">Go To Home Page</Link>
+        </div>
       </section>
     </div>
   );
