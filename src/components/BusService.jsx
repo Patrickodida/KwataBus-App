50d52ccee7c7fd61c283a47173918de50f1ecab2
Patrickodida <patrick.o.odida@gmail.com>
2024-06-11 13:26:07 +0300

    Modify the Bus Service component for responsiveness and button alignment

--- src/components/BusService.jsx
+++ src/components/BusService.jsx
@@ -20,7 +20,6 @@ function BusService(props) {
       <section className="flex md:justify-center flex-col md:flex-row bg-slate-100 p-4 rounded-lg text-center">
         <div className="text-center md:text-left">
           <h1 className="mb-4 font-bold">{props.busCompany}</h1>
-          {/* <h2 className="mb-4">Ordinary</h2> */}
           <p className="mb-4">
             <span className="font-bold">From</span> {props.departureTown}{" "}
             <span className="font-bold">To</span> {props.arrivalTown}
@@ -30,13 +29,15 @@ function BusService(props) {
         <div className="md:flex md:justify-center flex-col md:flex-row md:items-end">
           <p className="md:ml-8 mb-4">Departure time: {props.departureTime}</p>
           <p className="md:ml-8 mb-4">Cost: {props.fare}</p>
+          <div className="button flex justify-center w-full md:w-[50%]">
           <button
             onClick={handleSelectSeat}
-            className="ml-8 mb-2 bg-[#061f77] rounded-lg text-white py-2 px-10 text-center flex justify-center"
+            className="md:ml-8 md:mb-4 bg-[#061f77] rounded-lg text-white py-2 px-10 text-center w-full "
             style={{ whiteSpace: "nowrap" }}
           >
             Select Seat
           </button>
+          </div>
         </div>
       </section>
     </div>
